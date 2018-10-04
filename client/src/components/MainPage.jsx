94cc828ec216705e2a4a3d28193eff2f8d036187
Hannah Holzmann <hholzmann16@gmail.com>
2018-10-04 12:05:07 -0700

    Adding buttons to top

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Main from "../../dist/main.css";
 
 class MainPage extends React.Component {
   constructor(props) {
@@ -9,10 +10,28 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hannah Sophie Holzmann's Official Website</h1>
-        <marquee>
-          <h2 style={{ color: "#f442eb" }}> Hello World</h2>
-        </marquee>
+        <div>
+          <h1 className={Main.title}>
+            Hannah Sophie Holzmann's Official Website
+          </h1>
+          <div className={Main.container}>
+            <div className={Main.buttonContainer}>
+              <button className={Main.button}>Testing </button>
+            </div>
+            <div className={Main.buttonContainer}>
+              <button className={Main.button}>Testing </button>
+            </div>
+            <div className={Main.buttonContainer}>
+              <button className={Main.button}>Testing </button>
+            </div>
+            <div className={Main.buttonContainer}>
+              <button className={Main.button}>Testing </button>
+            </div>
+          </div>
+          <marquee>
+            <h2 style={{ color: "#f442eb" }}> Hello World</h2>
+          </marquee>
+        </div>
       </div>
     );
   }
