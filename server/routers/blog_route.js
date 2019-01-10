2a7c4fdbd24f30418d3d823264fdc65ab35a380e
Hannah Holzmann <hholzmann16@gmail.com>
2019-01-09 23:08:12 -0800

    Prevent others posting blogs

--- server/routers/blog_route.js
+++ server/routers/blog_route.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var blog_controller = require("../controllers/blog_controller.js");
 
 router.get("/", blog_controller.getPosts);
-router.post("/", blog_controller.createPosts);
-router.delete("/:id", blog_controller.deletePost);
+// router.post("/", blog_controller.createPosts);
+// router.delete("/:id", blog_controller.deletePost);
 router.get("/:id", blog_controller.getOnePost);
-router.put("/:id", blog_controller.editPost);
+// router.put("/:id", blog_controller.editPost);
 
 module.exports = router;
